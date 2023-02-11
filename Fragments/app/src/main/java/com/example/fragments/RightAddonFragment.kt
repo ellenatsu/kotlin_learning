86fb0578e465a1ed74ce28d9727fdd905408a899
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-10 22:36:29 -0800

    add  code

---
+++ Fragments/app/src/main/java/com/example/fragments/RightAddonFragment.kt
@@ -0,0 +1,17 @@
+package com.example.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RightAddonFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.right_addon_fragment, container, false)
+    }
+}
