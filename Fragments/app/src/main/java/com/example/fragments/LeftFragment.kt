5bcbdca04bf665269b7d27262d52ea6b3b5a7464
Ellen/Zhixuan <ellenatsuwang@gmail.com>
2023-02-13 19:38:09 -0800

    add viewbinding

--- Fragments/app/src/main/java/com/example/fragments/LeftFragment.kt
+++ Fragments/app/src/main/java/com/example/fragments/LeftFragment.kt
@@ -6,16 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
+import com.example.fragments.databinding.LeftFragmentBinding
 
 class LeftFragment : Fragment() {
+    //view binding
+    private var _binding: LeftFragmentBinding? = null
+    private val binding get() = _binding!!
     //onCreate
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.left_fragment, container, false)
+        _binding = LeftFragmentBinding.inflate(layoutInflater, container, false)
+        //assign button
+        binding.button1.onset
+        return binding.root
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 
 
 }
--- Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,27 +2,21 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import com.example.fragments.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.add_item -> Toast.makeText(this, "you clicked add", Toast.LENGTH_SHORT).show()
-            R.id.remove_item -> Toast.makeText(this, "you clicked remove", Toast.LENGTH_SHORT).show()
-        }
-        return true
-    }
 }
